f5e1be359527d7bc9b479f5e2416c811eab0f538
Pao Moua <mouap0330@gmail.com>
2018-10-06 11:43:27 -0700

    v4 - practice with objects

--- todo.js
+++ todo.js
@@ -1,30 +1,44 @@
-
+//v4
 
 var toDoList = {
-  todo: ['item 1', 'item 2', 'item 3'],
-  // it should have displayTodo method
+  todo: [],
+
   displayTodo: function() {
     console.log('My To Do:', this.todo);
   },
 
-  //method should add a new item at end of list
+  //method should add new objects to the array
   addAToDo: function(newTodoItem) {
-      this.todo.push(newTodoItem);
-      this.displayTodo();
+    this.todo.push({
+      toDoText: newTodoItem,
+      completed: false
+    });
+    this.displayTodo();
   },
-  //should edit/replace a value in the list
-  changeAToDo: function(position, newValue) {
-      // this.todo.splice(position, newValue);
-      this.todo[position] = newValue;
-      this.displayTodo();
+
+  //should change the toDoText property of object
+  changeAToDo: function(position, newTodoItem) {
+    this.todo[position].toDoText = newTodoItem;
+    this.displayTodo();
   },
-  //should delete the value at position indicated 
+
+  // flip value of 'completed' property
+  toggleCompleted: function(position) {
+    var todoVar = this.todo[position];
+    console.log('here' + todoVar);
+    todoVar.completed = !todoVar.completed;
+    this.displayTodo();
+  },
+
+  //should delete the value at position indicated
   deleteAToDo: function(position) {
-      this.todo.splice(position, 1);
-      this.displayTodo();
+    this.todo.splice(position, 1);
+    this.displayTodo();
   }
 };
 
-toDoList.addAToDo('plnkr');
-toDoList.changeAToDo(0, 'first item');
-toDoList.deleteAToDo(1,1);
+toDoList.addAToDo('test');
+toDoList.toggleCompleted(0);
+// toDoList.addAToDo('first');
+// toDoList.changeAToDo(0, 'second');
+// toDoList.deleteAToDo(1,1);
