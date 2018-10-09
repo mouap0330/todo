5aacc47d20a71f3e42b972dd41cfa573f7ec0615
Pao Moua <mouap0330@gmail.com>
2018-10-08 17:28:46 -0700

    v5

--- todo.js
+++ todo.js
@@ -1,15 +1,32 @@
-//v4
+//v5
+// displayTodo method should show toDoText property
+// displayTodo should tell you if todoArrays is empty
+// displayTodo should show completed property
+
 
 var toDoList = {
-  todo: [],
+  todoArray: [],
 
   displayTodo: function() {
-    console.log('My To Do:', this.todo);
+    console.log('My To Do:');
+
+    if (this.todoArray.length === 0) {
+        console.log('You have no todoArrays!')
+    } else {
+        for (var i = 0; i < this.todoArray.length; i++) {
+            // checks for completion
+            if (this.todoArray[i].completed === true) {
+                console.log('( x ) ' + this.todoArray[i].toDoText);
+            } else {
+                console.log('(   ) ' + this.todoArray[i].toDoText);
+            }
+        }
+    }
   },
 
   //method should add new objects to the array
   addAToDo: function(newTodoItem) {
-    this.todo.push({
+    this.todoArray.push({
       toDoText: newTodoItem,
       completed: false
     });
@@ -18,27 +35,27 @@ var toDoList = {
 
   //should change the toDoText property of object
   changeAToDo: function(position, newTodoItem) {
-    this.todo[position].toDoText = newTodoItem;
+    this.todoArray[position].toDoText = newTodoItem;
     this.displayTodo();
   },
 
   // flip value of 'completed' property
   toggleCompleted: function(position) {
-    var todoVar = this.todo[position];
-    console.log('here' + todoVar);
-    todoVar.completed = !todoVar.completed;
+    var todoArrayVar = this.todoArray[position];
+    console.log('here' + todoArrayVar);
+    todoArrayVar.completed = !todoArrayVar.completed;
     this.displayTodo();
   },
 
   //should delete the value at position indicated
   deleteAToDo: function(position) {
-    this.todo.splice(position, 1);
+    this.todoArray.splice(position, 1);
     this.displayTodo();
   }
 };
 
-toDoList.addAToDo('test');
-toDoList.toggleCompleted(0);
+// toDoList.addAToDo('test');
+// toDoList.toggleCompleted(0);
 // toDoList.addAToDo('first');
 // toDoList.changeAToDo(0, 'second');
 // toDoList.deleteAToDo(1,1);
